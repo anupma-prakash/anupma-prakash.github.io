adae29410795d94e1f72f8bfb4d2eb79ad9ab113
Rudi Gens <rgens@Rudis-MBP.gci.net>
2019-04-02 19:59:22 -0800

    Added Evapotranspiration website.

---
+++ evapotranspiration/index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Polar Remote Sensing Module</title>
+<link rel="stylesheet" href="styles/index.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="styles/print.css" type="text/css" media="print" />
+<!--[if gt IE 6]> <link rel="stylesheet" href="styles/index_ie.css" type="text/css"> <![endif]-->
+<link rel="icon" href="favicon.ico" type="image/x-icon"><link rel="shortcut icon" href="favicon.ico" type="image/x-icon"> 
+<script language="javascript" src="script/basic.js" type="text/javascript"></script>
+</head>
+
+<body>
+
+<!-- >> Page header here -->
+<div id="page_top"><img src="images/headers/h1-basic.jpg" alt="Polar RS Header" width="899" height="295" /></div>
+<!-- >> end header -->
+
+<div id="page_mid">
+
+<table  border="0" cellpadding="0" cellspacing="0" class="flushleft">
+<tr><td class="sidebar" valign="top">
+	  
+<div id="menublock"> <!-- >> Site Menu -->
+	<?php include("script/menu.html"); ?>
+</div> <!-- // menublock  -->	
+	
+</td><td class="maincontent" valign="top">
+<div id="content_area"><div id="content">
+
+<div id="printbuttons_intro"><a href="pdf/intro.pdf" target="_blank"></a> <a href="#" onclick="window.print();return false;" onmouseover="MM_swapImage('print','','images/buttons/print2.png',1)" onmouseout="MM_swapImgRestore()"><img src="images/buttons/print1.png" alt="print" name="print" width="72" height="24" border="0" id="print"  /></a></div>
+
+<!-- >> Page title here -->
+<div class="pgtitle">Evapotranspiration Sites</div>
+<!-- >> end page title -->
+  
+<!-- >> subtitle here -->  
+<div id="subt">
+<table border="0" cellspacing="0" cellpadding="0">
+<tr><td class="tabedges"><img src="images/subt_left2.png" alt="tab_left" width="6" height="24" /></td>
+<td class="subt_tab">Introduction </td>
+<td class="tabedges"><img src="images/subt_right2.png" alt="tab_right" width="6" height="24" /></td></tr>
+</table>
+</div>
+<!-- >> end subtitle --> 
+						
+<p>ET is a critical component of the hydrologic cycle and a process that will become more important as we witness increasing trends in climate warming, permafrost degradation, forest fire occurrences, & significant LCLU changes in Alaska.</p>
+<p>Significant improvements in our knowledge of magnitude and spatio-temporal variability in ET can be accomplished by using field measurements, remote sensing data, and modeling. </p>
+<p><strong>Evapotranspiration </strong></p>
+<p>ET is a critical component of the hydrologic cycle and a process that will become more important as we witness increasing trends in climate warming, permafrost degradation, forest fire occurrences, &amp; significant LCLU changes in Alaska.<br />
+<br /><br /></p>
+			
+<div id="subt">
+<table border="0" cellspacing="0" cellpadding="0">
+<tr><td class="tabedges"><img src="images/subt_left2.png" alt="tab_left" width="6" height="24" /></td>
+<td class="subt_tab">Study Area   </td>
+<td class="tabedges"><img src="images/subt_right2.png" alt="tab_right" width="6" height="24" /></td></tr>
+</table>
+</div>
+			
+<p>More info here. <br /><br /></p>
+		
+<!--<div id="case_study">
+<div id="case_img" style="background:url(images/main/link_rivers.png) no-repeat"></div>
+<div id="case_txt"><a href="javascript:;" class="imglinks" onmouseover="MM_swapImage('end3','','images/main/link_end_green.png',1)" onmouseout="MM_swapImgRestore()"><span class="case_txt_title">Arctic Rivers</span><br />
+Process multitemporal satellite images to study the dynamic Colville River in Northern Alaska.</a></div>
+<div id="case_end"><img src="images/main/link_end1.png" alt="end" name="end3" width="24" height="76" id="end3" /></div>
+</div>-->
+			
+</div> <!-- >> end content -->
+</div> <!-- >> end content_area -->
+	  
+</td></tr><tr>
+<td align="left" valign="bottom"></td>
+<td class="maincontent" valign="top"><div id="logoset"><img src="images/logos.jpg" alt="logos" name="logostrip" width="688" height="94" border="0" usemap="#logomap" id="logostrip" /></div></td>
+</tr></table>
+
+<map name="logoimgMapMap" id="logoimgMapMap">
+<area shape="rect" coords="36,178,123,222" href="http://www.gi.alaska.edu" target="_blank" />
+<area shape="rect" coords="32,10,124,51" href="http://www.uaf.edu/" target="_blank" />
+<area shape="rect" coords="32,63,123,107" href="http://www.uaf.edu/asgp/" target="_blank" />
+<area shape="rect" coords="35,122,122,166" href="http://esse21.usra.edu/" target="_blank" />
+</map>
+
+</div> <!-- >> end page_mid -->
+
+<div id="page_bot">
+	<div id="footer">		
+		<div class="copyright">Copyright 2011 Earth and Planetary Remote Sensing Group, UAF GI</div>
+	</div>
+</div> <!-- >> end page_bot -->
+
+</div>
+
+<map name="logomap" id="logomap"><area shape="rect" coords="21,14,90,82" href="http://www.nasa.gov/" target="_blank" />
+<area shape="rect" coords="109,14,180,82" href="http://www.gi.alaska.edu/" target="_blank" />
+<area shape="rect" coords="198,13,276,81" href="http://ine.uaf.edu/" target="_blank" />
+<area shape="rect" coords="292,15,386,81" href="http://www.asf.alaska.edu/" target="_blank" />
+<area shape="rect" coords="407,16,486,81" href="http://www.usda.gov/" target="_blank" />
+<area shape="rect" coords="574,12,668,84" href="http://www.uaf.edu/" target="_blank" />
+</map>
+
+</body>
+</html>
