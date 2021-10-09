2d543cde0e219de35f96edef8c735f447ed8a184
Rudiger Gens <rudi@Rudigers-MacBook-Pro.local>
2021-10-08 23:29:43 -0800

    Major update of the website. Added a leadership and administration section

---
+++ administration/expand.js
@@ -0,0 +1,512 @@
+function reset()
+{
+  document.getElementById("provost").style.display = 'inline';
+  document.getElementById("provost_expand").style.display = 'none';
+  document.getElementById("provost_collapse").style.display = 'none';
+  document.getElementById("epscor").style.display = 'inline';
+  document.getElementById("epscor_expand").style.display = 'none';
+  document.getElementById("epscor_collapse").style.display = 'none';
+  document.getElementById("dean").style.display = 'inline';
+  document.getElementById("dean_expand").style.display = 'none';
+  document.getElementById("dean_collapse").style.display = 'none';
+  document.getElementById("cdr").style.display = 'inline';
+  document.getElementById("cdr_expand").style.display = 'none';
+  document.getElementById("cdr_collapse").style.display = 'none';
+  document.getElementById("dept").style.display = 'inline';
+  document.getElementById("dept_expand").style.display = 'none';
+  document.getElementById("dept_collapse").style.display = 'none';
+  document.getElementById("rsgroup").style.display = 'inline';
+  document.getElementById("rsgroup_expand").style.display = 'none';
+  document.getElementById("rsgroup_collapse").style.display = 'none';
+  document.getElementById("itc").style.display = 'inline';
+  document.getElementById("itc_expand").style.display = 'none';
+  document.getElementById("itc_collapse").style.display = 'none';
+  document.getElementById("school").style.display = 'inline';
+  document.getElementById("school_expand").style.display = 'none';
+  document.getElementById("school_collapse").style.display = 'none';
+  document.getElementById("acc1").style.display = 'inline';
+  document.getElementById("acc1_expand").style.display = 'none';
+  document.getElementById("acc1_collapse").style.display = 'none';
+  document.getElementById("acc2").style.display = 'inline';
+  document.getElementById("acc2_expand").style.display = 'none';
+  document.getElementById("acc2_collapse").style.display = 'none';
+  document.getElementById("acc3").style.display = 'inline';
+  document.getElementById("acc3_expand").style.display = 'none';
+  document.getElementById("acc3_collapse").style.display = 'none';
+  document.getElementById("acc4").style.display = 'inline';
+  document.getElementById("acc4_expand").style.display = 'none';
+  document.getElementById("acc4_collapse").style.display = 'none';
+  document.getElementById("acc5").style.display = 'inline';
+  document.getElementById("acc5_expand").style.display = 'none';
+  document.getElementById("acc5_collapse").style.display = 'none';
+  document.getElementById("acc6").style.display = 'inline';
+  document.getElementById("acc6_expand").style.display = 'none';
+  document.getElementById("acc6_collapse").style.display = 'none';
+  document.getElementById("acc7").style.display = 'inline';
+  document.getElementById("acc7_expand").style.display = 'none';
+  document.getElementById("acc7_collapse").style.display = 'none';
+  document.getElementById("acc8").style.display = 'inline';
+  document.getElementById("acc8_expand").style.display = 'none';
+  document.getElementById("acc8_collapse").style.display = 'none';
+  document.getElementById("acc9").style.display = 'inline';
+  document.getElementById("acc9_expand").style.display = 'none';
+  document.getElementById("acc9_collapse").style.display = 'none';
+  document.getElementById("acc10").style.display = 'inline';
+  document.getElementById("acc10_expand").style.display = 'none';
+  document.getElementById("acc10_collapse").style.display = 'none';
+  document.getElementById("acc11").style.display = 'inline';
+  document.getElementById("acc11_expand").style.display = 'none';
+  document.getElementById("acc11_collapse").style.display = 'none';
+  document.getElementById("acc12").style.display = 'inline';
+  document.getElementById("acc12_expand").style.display = 'none';
+  document.getElementById("acc12_collapse").style.display = 'none';
+}
+
+function switch_init()
+{
+  if(document.getElementById) {
+    document.getElementById("provost").style.display = 'none';
+    document.getElementById("provost_expand").style.display = 'inline';
+    document.getElementById("provost_collapse").style.display = 'none';
+    document.getElementById("epscor").style.display = 'none';
+    document.getElementById("epscor_expand").style.display = 'inline';
+    document.getElementById("epscor_collapse").style.display = 'none';
+    document.getElementById("dean").style.display = 'none';
+    document.getElementById("dean_expand").style.display = 'inline';
+    document.getElementById("dean_collapse").style.display = 'none';
+    document.getElementById("cdr").style.display = 'none';
+    document.getElementById("cdr_expand").style.display = 'inline';
+    document.getElementById("cdr_collapse").style.display = 'none';
+    document.getElementById("dept").style.display = 'none';
+    document.getElementById("dept_expand").style.display = 'inline';
+    document.getElementById("dept_collapse").style.display = 'none';
+    document.getElementById("rsgroup").style.display = 'none';
+    document.getElementById("rsgroup_expand").style.display = 'inline';
+    document.getElementById("rsgroup_collapse").style.display = 'none';
+    document.getElementById("itc").style.display = 'none';
+    document.getElementById("itc_expand").style.display = 'inline';
+    document.getElementById("itc_collapse").style.display = 'none';
+    document.getElementById("school").style.display = 'none';
+    document.getElementById("school_expand").style.display = 'inline';
+    document.getElementById("school_collapse").style.display = 'none';
+    document.getElementById("acc1").style.display = 'none';
+    document.getElementById("acc1_expand").style.display = 'inline';
+    document.getElementById("acc1_collapse").style.display = 'none';
+    document.getElementById("acc2").style.display = 'none';
+    document.getElementById("acc2_expand").style.display = 'inline';
+    document.getElementById("acc2_collapse").style.display = 'none';
+    document.getElementById("acc3").style.display = 'none';
+    document.getElementById("acc3_expand").style.display = 'inline';
+    document.getElementById("acc3_collapse").style.display = 'none';
+    document.getElementById("acc4").style.display = 'none';
+    document.getElementById("acc4_expand").style.display = 'inline';
+    document.getElementById("acc4_collapse").style.display = 'none';
+    document.getElementById("acc5").style.display = 'none';
+    document.getElementById("acc5_expand").style.display = 'inline';
+    document.getElementById("acc5_collapse").style.display = 'none';
+    document.getElementById("acc6").style.display = 'none';
+    document.getElementById("acc6_expand").style.display = 'inline';
+    document.getElementById("acc6_collapse").style.display = 'none';
+    document.getElementById("acc7").style.display = 'none';
+    document.getElementById("acc7_expand").style.display = 'inline';
+    document.getElementById("acc7_collapse").style.display = 'none';
+    document.getElementById("acc8").style.display = 'none';
+    document.getElementById("acc8_expand").style.display = 'inline';
+    document.getElementById("acc8_collapse").style.display = 'none';
+    document.getElementById("acc9").style.display = 'none';
+    document.getElementById("acc9_expand").style.display = 'inline';
+    document.getElementById("acc9_collapse").style.display = 'none';
+    document.getElementById("acc10").style.display = 'none';
+    document.getElementById("acc10_expand").style.display = 'inline';
+    document.getElementById("acc10_collapse").style.display = 'none';
+    document.getElementById("acc11").style.display = 'none';
+    document.getElementById("acc11_expand").style.display = 'inline';
+    document.getElementById("acc11_collapse").style.display = 'none';
+    document.getElementById("acc12").style.display = 'none';
+    document.getElementById("acc12_expand").style.display = 'inline';
+    document.getElementById("acc12_collapse").style.display = 'none';
+  }
+  else {
+    reset();
+  }
+}
+
+function switch_provost(opt)
+{
+  if (document.getElementById) {
+    if (opt == 'on') {
+      document.getElementById("provost").style.display = 'inline';
+      document.getElementById("provost_expand").style.display = 'none';
+      document.getElementById("provost_collapse").style.display = 'inline';
+    }
+    if (opt == 'off') {
+      document.getElementById("provost").style.display = 'none';
+      document.getElementById("provost_expand").style.display = 'inline';
+      document.getElementById("provost_collapse").style.display = 'none';
+    }
+  }
+  else {
+    reset();
+  }
+}
+
+function switch_epscor(opt)
+{
+  if (document.getElementById) {
+    if (opt == 'on') {
+      document.getElementById("epscor").style.display = 'inline';
+      document.getElementById("epscor_expand").style.display = 'none';
+      document.getElementById("epscor_collapse").style.display = 'inline';
+    }
+    if (opt == 'off') {
+      document.getElementById("epscor").style.display = 'none';
+      document.getElementById("epscor_expand").style.display = 'inline';
+      document.getElementById("epscor_collapse").style.display = 'none';
+    }
+  }
+  else {
+    reset();
+  }
+}
+
+function switch_dean(opt)
+{
+  if (document.getElementById) {
+    if (opt == 'on') {
+      document.getElementById("dean").style.display = 'inline';
+      document.getElementById("dean_expand").style.display = 'none';
+      document.getElementById("dean_collapse").style.display = 'inline';
+    }
+    if (opt == 'off') {
+      document.getElementById("dean").style.display = 'none';
+      document.getElementById("dean_expand").style.display = 'inline';
+      document.getElementById("dean_collapse").style.display = 'none';
+    }
+  }
+  else {
+    reset();
+  }
+}
+
+function switch_cdr(opt)
+{
+  if (document.getElementById) {
+    if (opt == 'on') {
+      document.getElementById("cdr").style.display = 'inline';
+      document.getElementById("cdr_expand").style.display = 'none';
+      document.getElementById("cdr_collapse").style.display = 'inline';
+    }
+    if (opt == 'off') {
+      document.getElementById("cdr").style.display = 'none';
+      document.getElementById("cdr_expand").style.display = 'inline';
+      document.getElementById("cdr_collapse").style.display = 'none';
+    }
+  }
+  else {
+    reset();
+  }
+}
+
+function switch_dept(opt)
+{
+  if (document.getElementById) {
+    if (opt == 'on') {
+      document.getElementById("dept").style.display = 'inline';
+      document.getElementById("dept_expand").style.display = 'none';
+      document.getElementById("dept_collapse").style.display = 'inline';
+    }
+    if (opt == 'off') {
+      document.getElementById("dept").style.display = 'none';
+      document.getElementById("dept_expand").style.display = 'inline';
+      document.getElementById("dept_collapse").style.display = 'none';
+    }
+  }
+  else {
+    reset();
+  }
+}
+
+function switch_rsgroup(opt)
+{
+  if (document.getElementById) {
+    if (opt == 'on') {
+      document.getElementById("rsgroup").style.display = 'inline';
+      document.getElementById("rsgroup_expand").style.display = 'none';
+      document.getElementById("rsgroup_collapse").style.display = 'inline';
+    }
+    if (opt == 'off') {
+      document.getElementById("rsgroup").style.display = 'none';
+      document.getElementById("rsgroup_expand").style.display = 'inline';
+      document.getElementById("rsgroup_collapse").style.display = 'none';
+    }
+  }
+  else {
+    reset();
+  }
+}
+
+function switch_itc(opt)
+{
+  if (document.getElementById) {
+    if (opt == 'on') {
+      document.getElementById("itc").style.display = 'inline';
+      document.getElementById("itc_expand").style.display = 'none';
+      document.getElementById("itc_collapse").style.display = 'inline';
+    }
+    if (opt == 'off') {
+      document.getElementById("itc").style.display = 'none';
+      document.getElementById("itc_expand").style.display = 'inline';
+      document.getElementById("itc_collapse").style.display = 'none';
+    }
+  }
+  else {
+    reset();
+  }
+}
+
+function switch_school(opt)
+{
+  if (document.getElementById) {
+    if (opt == 'on') {
+      document.getElementById("school").style.display = 'inline';
+      document.getElementById("school_expand").style.display = 'none';
+      document.getElementById("school_collapse").style.display = 'inline';
+    }
+    if (opt == 'off') {
+      document.getElementById("school").style.display = 'none';
+      document.getElementById("school_expand").style.display = 'inline';
+      document.getElementById("school_collapse").style.display = 'none';
+    }
+  }
+  else {
+    reset();
+  }
+}
+
+function switch_acc1(opt)
+{
+  if (document.getElementById) {
+    if (opt == 'on') {
+      document.getElementById("acc1").style.display = 'inline';
+      document.getElementById("acc1_expand").style.display = 'none';
+      document.getElementById("acc1_collapse").style.display = 'inline';
+    }
+    if (opt == 'off') {
+      document.getElementById("acc1").style.display = 'none';
+      document.getElementById("acc1_expand").style.display = 'inline';
+      document.getElementById("acc1_collapse").style.display = 'none';
+    }
+  }
+  else {
+    reset();
+  }
+}
+
+function switch_acc2(opt)
+{
+  if (document.getElementById) {
+    if (opt == 'on') {
+      document.getElementById("acc2").style.display = 'inline';
+      document.getElementById("acc2_expand").style.display = 'none';
+      document.getElementById("acc2_collapse").style.display = 'inline';
+    }
+    if (opt == 'off') {
+      document.getElementById("acc2").style.display = 'none';
+      document.getElementById("acc2_expand").style.display = 'inline';
+      document.getElementById("acc2_collapse").style.display = 'none';
+    }
+  }
+  else {
+    reset();
+  }
+}
+
+function switch_acc3(opt)
+{
+  if (document.getElementById) {
+    if (opt == 'on') {
+      document.getElementById("acc3").style.display = 'inline';
+      document.getElementById("acc3_expand").style.display = 'none';
+      document.getElementById("acc3_collapse").style.display = 'inline';
+    }
+    if (opt == 'off') {
+      document.getElementById("acc3").style.display = 'none';
+      document.getElementById("acc3_expand").style.display = 'inline';
+      document.getElementById("acc3_collapse").style.display = 'none';
+    }
+  }
+  else {
+    reset();
+  }
+}
+
+function switch_acc4(opt)
+{
+  if (document.getElementById) {
+    if (opt == 'on') {
+      document.getElementById("acc4").style.display = 'inline';
+      document.getElementById("acc4_expand").style.display = 'none';
+      document.getElementById("acc4_collapse").style.display = 'inline';
+    }
+    if (opt == 'off') {
+      document.getElementById("acc4").style.display = 'none';
+      document.getElementById("acc4_expand").style.display = 'inline';
+      document.getElementById("acc4_collapse").style.display = 'none';
+    }
+  }
+  else {
+    reset();
+  }
+}
+
+function switch_acc5(opt)
+{
+  if (document.getElementById) {
+    if (opt == 'on') {
+      document.getElementById("acc5").style.display = 'inline';
+      document.getElementById("acc5_expand").style.display = 'none';
+      document.getElementById("acc5_collapse").style.display = 'inline';
+    }
+    if (opt == 'off') {
+      document.getElementById("acc5").style.display = 'none';
+      document.getElementById("acc5_expand").style.display = 'inline';
+      document.getElementById("acc5_collapse").style.display = 'none';
+    }
+  }
+  else {
+    reset();
+  }
+}
+
+function switch_acc6(opt)
+{
+  if (document.getElementById) {
+    if (opt == 'on') {
+      document.getElementById("acc6").style.display = 'inline';
+      document.getElementById("acc6_expand").style.display = 'none';
+      document.getElementById("acc6_collapse").style.display = 'inline';
+    }
+    if (opt == 'off') {
+      document.getElementById("acc6").style.display = 'none';
+      document.getElementById("acc6_expand").style.display = 'inline';
+      document.getElementById("acc6_collapse").style.display = 'none';
+    }
+  }
+  else {
+    reset();
+  }
+}
+
+function switch_acc7(opt)
+{
+  if (document.getElementById) {
+    if (opt == 'on') {
+      document.getElementById("acc7").style.display = 'inline';
+      document.getElementById("acc7_expand").style.display = 'none';
+      document.getElementById("acc7_collapse").style.display = 'inline';
+    }
+    if (opt == 'off') {
+      document.getElementById("acc7").style.display = 'none';
+      document.getElementById("acc7_expand").style.display = 'inline';
+      document.getElementById("acc7_collapse").style.display = 'none';
+    }
+  }
+  else {
+    reset();
+  }
+}
+
+function switch_acc8(opt)
+{
+  if (document.getElementById) {
+    if (opt == 'on') {
+      document.getElementById("acc8").style.display = 'inline';
+      document.getElementById("acc8_expand").style.display = 'none';
+      document.getElementById("acc8_collapse").style.display = 'inline';
+    }
+    if (opt == 'off') {
+      document.getElementById("acc8").style.display = 'none';
+      document.getElementById("acc8_expand").style.display = 'inline';
+      document.getElementById("acc8_collapse").style.display = 'none';
+    }
+  }
+  else {
+    reset();
+  }
+}
+
+function switch_acc9(opt)
+{
+  if (document.getElementById) {
+    if (opt == 'on') {
+      document.getElementById("acc9").style.display = 'inline';
+      document.getElementById("acc9_expand").style.display = 'none';
+      document.getElementById("acc9_collapse").style.display = 'inline';
+    }
+    if (opt == 'off') {
+      document.getElementById("acc9").style.display = 'none';
+      document.getElementById("acc9_expand").style.display = 'inline';
+      document.getElementById("acc9_collapse").style.display = 'none';
+    }
+  }
+  else {
+    reset();
+  }
+}
+
+function switch_acc10(opt)
+{
+  if (document.getElementById) {
+    if (opt == 'on') {
+      document.getElementById("acc10").style.display = 'inline';
+      document.getElementById("acc10_expand").style.display = 'none';
+      document.getElementById("acc10_collapse").style.display = 'inline';
+    }
+    if (opt == 'off') {
+      document.getElementById("acc10").style.display = 'none';
+      document.getElementById("acc10_expand").style.display = 'inline';
+      document.getElementById("acc10_collapse").style.display = 'none';
+    }
+  }
+  else {
+    reset();
+  }
+}
+
+function switch_acc11(opt)
+{
+  if (document.getElementById) {
+    if (opt == 'on') {
+      document.getElementById("acc11").style.display = 'inline';
+      document.getElementById("acc11_expand").style.display = 'none';
+      document.getElementById("acc11_collapse").style.display = 'inline';
+    }
+    if (opt == 'off') {
+      document.getElementById("acc11").style.display = 'none';
+      document.getElementById("acc11_expand").style.display = 'inline';
+      document.getElementById("acc11_collapse").style.display = 'none';
+    }
+  }
+  else {
+    reset();
+  }
+}
+
+function switch_acc12(opt)
+{
+  if (document.getElementById) {
+    if (opt == 'on') {
+      document.getElementById("acc12").style.display = 'inline';
+      document.getElementById("acc12_expand").style.display = 'none';
+      document.getElementById("acc12_collapse").style.display = 'inline';
+    }
+    if (opt == 'off') {
+      document.getElementById("acc12").style.display = 'none';
+      document.getElementById("acc12_expand").style.display = 'inline';
+      document.getElementById("acc12_collapse").style.display = 'none';
+    }
+  }
+  else {
+    reset();
+  }
+}
